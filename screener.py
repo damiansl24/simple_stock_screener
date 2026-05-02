ef96204897179f263657069e56a4743fac5a058c
Damian L <souhoonlee@gmail.com>
2026-05-02 00:45:25 -0500

    Added load and typ price functions: got too tired, am going to sleep.

--- screener.py
+++ screener.py
@@ -1,6 +1,27 @@
 import yfinance as yf
 import pandas as pd
 
+def typical_price(price: pd.Series) -> list:
+    '''
+    Calculates typical price of an index on a certain day. 
+
+    Parameters: 
+    price: A DataFrame object with ticker price data
+
+    Returns: 
+    Series of typical prices for the tickers.
+    '''
+    tickers: list[str] = price.index.get_level_values(1).unique().tolist()
+
+    typical_prices: list[float] = []
+
+    # input is a series of the COHL prices for one day per ticker. 
+    for t in tickers:
+        tp = (price["Close", t]  + price["High", t] + price["Low", t]) / 3
+        typical_prices.append(tp)
+
+    return typical_prices
+
 def load(ticker: list[str] | str, start: str, end: str) -> pd.Series | pd.DataFrame:
     '''
     loads price history of a stock ticker (or list of stock tickers). 
@@ -14,7 +35,17 @@ def load(ticker: list[str] | str, start: str, end: str) -> pd.Series | pd.DataFr
     A series or Dataframe of daily typical price data for the given tickers.
     '''
 
-    raise NotImplementedError
+    df = yf.download(ticker, start, end)
+
+    if type(ticker) == list:
+        for t in ticker:
+            df["typ", t] = df.apply(lambda row: typical_price(row)[tickers.index(t)], axis = 1)
+    else:
+        df["typ", t] = df.apply(lambda row: typical_price(row), axis = 1).
+
+    return df["typ"]
+
+
 
 def ret(mkt_data: pd.DataFrame | pd.Series) -> pd.Series | pd.DataFrame:
     '''
@@ -26,8 +57,8 @@ def ret(mkt_data: pd.DataFrame | pd.Series) -> pd.Series | pd.DataFrame:
     Returns:
     A series or dataframe showing daily percentage returns for some tickers.
     '''
+    
 
-    raise NotImplementedError
 
 def sharpe(mkt_data: pd.DataFrame | pd.Series) -> list[float]:
     '''
